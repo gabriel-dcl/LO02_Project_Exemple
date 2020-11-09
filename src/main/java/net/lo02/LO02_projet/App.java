b7b14d0055ed268da6e8dc425a831c0a43211ab9
gabriel-dcl <gabriel-dcl@users.noreply.github.com>
2020-11-09 14:59:28 +0100

    First commit

---
+++ src/main/java/net/lo02/LO02_projet/App.java
@@ -0,0 +1,13 @@
+package net.lo02.LO02_projet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
